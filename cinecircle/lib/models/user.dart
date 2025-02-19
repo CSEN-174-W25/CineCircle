37b28724bcf0687edc690699a72be66c4cfcb581
Ruby Huynh <huynhruby12@gmail.com>
2025-02-19 16:39:50 -0600

    Added User Model

---
+++ cinecircle/lib/models/user.dart
@@ -0,0 +1,19 @@
+class User{
+    final String userId;
+    String username;
+    double averageRating;
+
+    User({
+        required.this.userId,
+        required.this.username,
+        this.averageRating
+    })
+
+    factory User.fromJson(Map<String, dynamic> json){
+        return User(
+            userId: json['userId'],
+            username: json['username'],
+            averageRating:  (json['score'] as num).toDouble()
+        );
+    }
+}
